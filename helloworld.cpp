e3b13002464cb872573134a8bf4a583ec6599b81
Amy <arene.huang@gmail.com>
2024-10-11 18:07:09 +0800

    added comments

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,16 +1,22 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <iostream> //includes the standard input-output stream libary: cin (receving input), cout (print output), cerr
+#include <vector> //vector libary to use vector class, vector: dynamic array
+#include <string> //support for the string class, easier tha c-style char array (char[])
 
-using namespace std;
+using namespace std; //allows you to use standard names like cout w/out the std:: prefix
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+int main() { //entry point of C++ program, return type int
+
+    //create a vector of elements of type string called msg, holds 6 string elements
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"}; //initialize list w strings
     
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
-}
+    //range-based for loop
+    for (const string& word : msg) { //iterates through msg vector
+    //cost: ensure word cannot be  modified inside loop
+    //& word passed by REFERENCE: more efficient, avoids making copy of each string element
+        cout << word << " "; //outputs each word to the console w/ cout
+    } //for
+
+    cout << endl; //prints newline character to console
+
+    return 0; //for main, indicates program ended sucessfully
+} //main
